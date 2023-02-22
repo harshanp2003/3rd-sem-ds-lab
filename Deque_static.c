99adf7e4e0b7e6798dda9cbebbf85be8d68c4569
harshanp2003 <111213910+harshanp2003@users.noreply.github.com>
2023-02-22 23:12:49 +0530

    Create Deque_static.c

---
+++ Deque_static.c
@@ -0,0 +1,106 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct Q
+{
+    int f;
+    int size;
+    int r;
+    int A[100];
+};
+void delr(struct Q *q)
+{
+    if(q->r==-1 || q->r<q->f)
+    {
+        printf("Deletion not possible\n");
+        return;
+    }
+    int x;
+    x=q->A[q->r];
+    q->r--;
+    printf("%d is the deleted element\n",x);
+}
+void insertf(struct Q *q,int data)
+{ 
+   if(q->f==0 && q->r==-1)
+   {
+       q->A[++q->r]=data;
+       return;
+   }
+   if(q->f!=0)
+   {
+       q->A[--q->f]=data;
+       return;
+   }
+   printf("Insertion not possible\n");
+   
+}
+void enqu(struct Q *q,int data)
+{
+ 
+    if(q->r==q->size-1)
+       printf("Overflow\n");
+    else
+       q->A[++(q->r)]=data;
+}
+void deque(struct Q *q)
+{
+    int x;
+     if(q->f > q->r)
+     {
+          printf("Deletion not possible\n");
+          q->f=0;q->r=-1;
+     }
+       
+     else
+        {
+            x=q->A[q->f++];
+            printf("Deleted element is %d \n",x);
+        }
+    
+    
+}
+void display(struct Q *q)
+{
+    int i;
+    for(i=q->f;i<=q->r;i++)
+    {
+        printf("%d ",q->A[i]);
+    }
+    printf("\n");
+}
+void main()
+{
+    int n,ch;
+    struct Q q;
+    q.f=0;
+    q.r=-1;
+    printf("Enter the value of q size\n");
+    scanf("%d",&q.size);
+    while(1)
+    {
+        printf("1.Insertf\n2.Insert\n3.Delete\n4.Display\n5.Exit\n6.deleterear\n");
+        printf("Enter your choice\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1: printf("Enter the value of n\n");
+                    scanf("%d",&n);
+                    insertf(&q,n);
+                    break;
+            case 2: printf("Enter n\n");
+                    scanf("%d",&n);
+                    enqu(&q,n);
+                    break;
+            case 3:deque(&q);
+                   break;
+            case 4:display(&q);
+                   break;
+            case 5:exit(0);
+            case 6: delr(&q);
+                    break;
+                
+            default:printf("Invalid Choice\n");
+                    break;
+        }
+    }
+}
