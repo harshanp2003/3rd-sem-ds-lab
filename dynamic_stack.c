393271c5dd155dd4097ad4b254f950adf3d3ae9b
harshanp2003 <111213910+harshanp2003@users.noreply.github.com>
2023-02-22 22:22:16 +0530

    Create dynamic_stack.c

---
+++ dynamic_stack.c
@@ -0,0 +1,87 @@
+ #include<stdio.h>
+#include<stdlib.h>
+struct stack
+{
+    int top;
+    int size;
+    int *s;
+    
+};
+
+void Pop(struct stack *st )
+{
+    int y;
+    if(st->top==-1)
+    {
+        printf("Stack Underflow\n");
+    }
+    else
+    {
+        y=st->s[st->top];
+        st->top--;
+        printf("\n%d is the popped element\n",y);
+    }
+}
+void Push(struct stack *st,int x)
+{
+    // st->top=-1;
+    if(st->top==st->size-1)
+    {
+         printf("Stack Overflow\n");
+    }
+   
+    else
+    {
+        st->top++;
+        st->s[st->top]=x;
+    }
+}
+void Create(struct stack *st)
+{
+    int size;
+    printf("Enter the size\n");
+    scanf("%d",&st->size);
+    st->top=-1;
+    st->s=(int *)malloc(sizeof(int)*st->size);
+}
+void Display(struct stack *st)
+{
+    int i;
+    for(i=0;i<=st->top;i++)
+    {
+        printf("%d ",st->s[i]);
+    }
+}
+int main()
+{
+    struct stack st;
+    
+     int index,r,ch,n;
+     while(1)
+     {
+         printf("\n0.Create\n1.Push\n2.Display\n3.Pop\n4.exit\n");
+         printf("Enter your choice\n ");
+         scanf("%d",&ch);
+         switch(ch)
+         {
+             case 0:
+                    Create(&st);
+                    break;
+             case 1:
+                    printf("\nEnter the element\n");
+                    scanf("%d",&n);
+                    Push(&st,n);
+                    break;
+            case 2:
+                    Display(&st);
+                    break;
+            case 3:
+                   Pop(&st);
+                   break;
+            case 4:
+                   exit(0);
+                   break;
+           
+         }
+     }
+}    
